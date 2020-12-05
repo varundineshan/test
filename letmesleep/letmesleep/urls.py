52fc6684f0dbc9aa04051dcf9db3ffd0e176b1ac
varun dineshan <varun4dinesh@gmail.com>
2020-12-06 06:09:58 +0800

    next change

--- letmesleep/letmesleep/urls.py
+++ letmesleep/letmesleep/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    #this is another change
     path('admin/', admin.site.urls),
     path('', include('my_application.urls')),
         path('varun/', include('my_application.urls')),
